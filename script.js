bebb22c411c6e718d9594daf1ac2bb6d11673efd
GabrielPeixinho11ATI GabrielPeixinho11ATI <gabrielpeixinh016@gmail.com>
2024-04-21 21:51:32 +0000

    javascript do index

---
+++ script.js
@@ -0,0 +1,10 @@
+let botoes = document.querySelectorAll(".botoes button")
+botoes.forEach(botao=>{
+    botao.addEventListener("click", gurdarNota)
+})
+
+function gurdarNota() {
+    let nota = evento.target.innerText
+
+   localStorage.setItem("nota",nota)
+}
